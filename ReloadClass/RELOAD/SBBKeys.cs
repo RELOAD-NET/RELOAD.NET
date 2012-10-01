86f05c4f332d0e6d4d37010b0be4825674f93349
Joscha Schneider <j.schneider@hs-mannheim.de>
2012-10-01 11:42:11 +0200

    test2

---
+++ ReloadClass/RELOAD/SBBKeys.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TSystems {
+  class SBBKeys {
+    public static string SBB_LICENSE_SBB8_KEY = "put your (eval) license here";
+    public static string SBB_LICENSE_PKI8_KEY = "put your (eval) license here";
+  }
+}
